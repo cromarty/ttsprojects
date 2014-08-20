480ba314809949cd1c2de235962b817abe19f081
Mike Ray <mike.ray@btinternet.com>
2014-08-20 23:40:08 +0000

    On branch master
    New file:   espeak.c

---
+++ raspberry-pi-audio/libilctts/src/espeak.c
@@ -0,0 +1,12 @@
+
+
+
+/*
+* Return values:
+*   0 = continnue synthesis
+*   1 = abort synthesis
+*/
+int espeak_synth_callback(short *wav, int numsamples, espeak_EVENT *events) {
+	return 0;
+} // end espeak_synth_callback
+
