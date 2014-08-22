d0eb9d4f53388636ebbb71e2e74acb38f6b81ae8
Mike Ray <mike.ray@btinternet.com>
2014-08-22 10:35:59 +0000

    Added linked list and queue source files to libilctts/src

---
+++ raspberry-pi-audio/libilctts/src/queue.h
@@ -0,0 +1,22 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdlib.h>
+
+#include "list.h"
+
+
+typedef LLIST_T QUEUE_T;
+
+
+#define queue_init list_init
+#define queue_destroy list_destroy
+
+int queue_enqueue(QUEUE_T *queue, const void *data);
+int queue_dequeue(QUEUE_T *queue, void **data);
+
+#define queue_peek(queue) ((queue)->head == NULL ? NULL : (queue)->head->data)
+#define queue_is_empty(list) (list_is_empty(list))
+#define queue_size list_size
+
+#endif
