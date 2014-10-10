c0e037374e09762c110719adc7f0c2ef18a8cb77
Mike Ray <mike.ray@btinternet.com>
2014-10-10 20:59:43 +0000

    Changes to espeak.c in libilctts

--- raspberry-pi-audio/libilctts/src/espeak.c
+++ raspberry-pi-audio/libilctts/src/espeak.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#include "list.h"
+#include "queue.h"
+#include "ilctts_lib.h"
+#include "espeak.h"
 
 
 static int synth_callback(short *wav, int numsamples, espeak_EVENT * events) {
