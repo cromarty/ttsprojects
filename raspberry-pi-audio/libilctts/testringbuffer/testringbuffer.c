37f62407a928034c3c17417424c28fb6e16c6d1f
Mike Ray <mike.ray@btinternet.com>
2014-10-19 15:50:44 +0000

    Created testring stuff in raspberry-pi-audio/libilctts/testringbuffer/

---
+++ raspberry-pi-audio/libilctts/testringbuffer/testringbuffer.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "ringbuffer.h"
+
+void print_ringbuffer_state(RING_BUFFER_T *rb) {
+	return;
+}
+
+int main(int argc, char **argv) {
+	RING_BUFFER_T *rb;
+	return 0;
+}
+
