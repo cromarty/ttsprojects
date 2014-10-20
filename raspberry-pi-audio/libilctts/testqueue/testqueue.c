f99c28e211a3afbd02e5dc33912554832b99d1b1
Mike Ray <mike.ray@btinternet.com>
2014-10-20 16:07:18 +0000

    Changes to Makefile and testqueue.c in testqueue

--- raspberry-pi-audio/libilctts/testqueue/testqueue.c
+++ raspberry-pi-audio/libilctts/testqueue/testqueue.c
@@ -6,122 +6,106 @@
 
 
 static void print_queue(const QUEUE_T *queue) {
-
-LLIST_ENTRY_T           *element;
-
-int                *data,
-                   size,
-                   i;
-
-fprintf(stdout, "QUEUE_T size is %d\n", size = queue_size(queue));
-
-i = 0;
-element = list_head(queue);
-
-while (i < size) {
-
-   data = list_data(element);
-   fprintf(stdout, "queue[%03d]=%03d\n", i, *data);
-   element = list_next(element);
-   i++;
-
-}
-
-return;
-
-}
+	LLIST_ENTRY_T *element;
+	int *data, size, i;
+	fprintf(stdout, "QUEUE_T size is %d\n", size = queue_size(queue));
+	i = 0;
+	element = list_head(queue);
+	while (i < size) {
+		data = list_data(element);
+		fprintf(stdout, "queue[%03d]=%03d\n", i, *data);
+		element = list_next(element);
+		i++;
+	}
+	return;
+} // end print_queue
 
 int main(int argc, char **argv) {
-QUEUE_T              queue;
-int                *data, i;
+	QUEUE_T              queue;
+	int                *data, i;
 
-queue_init(&queue, free);
+	queue_init(&queue, free);
 	if (queue_is_empty(&queue))
 		fprintf(stdout, "Queue is empty\n");
 	else
 		fprintf(stdout, "Queue is not empty\n");
 
-fprintf(stdout, "Enqueuing 10 elements\n");
+	fprintf(stdout, "Enqueuing 10 elements\n");
 
-for (i = 0; i < 10; i++) {
-   if ((data = (int *)malloc(sizeof(int))) == NULL)
-      return 1;
+	for (i = 0; i < 10; i++) {
+		if ((data = (int *)malloc(sizeof(int))) == NULL)
+			return 1;
 
-   *data = i + 1;
+		*data = i + 1;
 
-   if (queue_enqueue(&queue, data) != 0)
-      return 1;
+		if (queue_enqueue(&queue, data) != 0)
+			return 1;
 
-}
+	}
 
-print_queue(&queue);
+	print_queue(&queue);
 	fprintf(stdout, "Checking if the queue is empty\n");
 	if (queue_is_empty(&queue))
 		fprintf(stdout, "Queue is empty\n");
 	else
 		fprintf(stdout, "Queue is not empty\n");
 
-fprintf(stdout, "Dequeuing 5 elements\n");
+	fprintf(stdout, "Dequeuing 5 elements\n");
 
-for (i = 0; i < 5; i++) {
-   if (queue_dequeue(&queue, (void **)&data) == 0)
-      free(data);
-   else
-      return 1;
+	for (i = 0; i < 5; i++) {
+		if (queue_dequeue(&queue, (void **)&data) == 0)
+			free(data);
+		else
+			return 1;
 
-}
+	}
 
-print_queue(&queue);
+	print_queue(&queue);
+	fprintf(stdout, "Enqueuing 100 and 200\n");
 
-fprintf(stdout, "Enqueuing 100 and 200\n");
+	if ((data = (int *)malloc(sizeof(int))) == NULL)
+		return 1;
 
-if ((data = (int *)malloc(sizeof(int))) == NULL)
-   return 1;
+	*data = 100;
+	if (queue_enqueue(&queue, data) != 0)
+		return 1;
 
-*data = 100;
+	if ((data = (int *)malloc(sizeof(int))) == NULL)
+		return 1;
 
-if (queue_enqueue(&queue, data) != 0)
-   return 1;
+	*data = 200;
+	if (queue_enqueue(&queue, data) != 0)
+		return 1;
 
-if ((data = (int *)malloc(sizeof(int))) == NULL)
-   return 1;
+	print_queue(&queue);
+	if ((data = queue_peek(&queue)) != NULL)
+		fprintf(stdout, "Peeking at the head element...Value=%03d\n", *data);
+	else
+		fprintf(stdout, "Peeking at the head element...Value=NULL\n");
 
-*data = 200;
+	print_queue(&queue);
+	fprintf(stdout, "Dequeuing all elements\n");
 
-if (queue_enqueue(&queue, data) != 0)
-   return 1;
-
-print_queue(&queue);
-
-if ((data = queue_peek(&queue)) != NULL)
-   fprintf(stdout, "Peeking at the head element...Value=%03d\n", *data);
-else
-   fprintf(stdout, "Peeking at the head element...Value=NULL\n");
-
-print_queue(&queue);
-
-fprintf(stdout, "Dequeuing all elements\n");
-
-while (queue_size(&queue) > 0) {
-
-   if (queue_dequeue(&queue, (void **)&data) == 0)
-      free(data);
+	while (queue_size(&queue) > 0) {
+		if (queue_dequeue(&queue, (void **)&data) == 0)
+			free(data);
    
-}
+	}
 
-if ((data = queue_peek(&queue)) != NULL)
-   fprintf(stdout, "Peeking at an empty queue...Value=%03d\n", *data);
-else
-   fprintf(stdout, "Peeking at an empty queue...Value=NULL\n");
+	if ((data = queue_peek(&queue)) != NULL)
+		fprintf(stdout, "Peeking at an empty queue...Value=%03d\n", *data);
+	else
+		fprintf(stdout, "Peeking at an empty queue...Value=NULL\n");
+
 	fprintf(stdout, "Checking to see if the queue is empty\n"); 
 	if (queue_is_empty(&queue))
 		fprintf(stdout, "Queue is empty\n");
 	else
 		fprintf(stdout, "Queue is not empty\n");
 
-fprintf(stdout, "Destroying the queue\n");
-queue_destroy(&queue);
+	fprintf(stdout, "Destroying the queue\n");
+	queue_destroy(&queue);
+	return 0;
 
-return 0;
+} // end main
 
-}
