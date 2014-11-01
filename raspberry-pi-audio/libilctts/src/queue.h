446a0d06c119cec67c0b850acd1a9648105581b2
Mike Ray <mike.ray@btinternet.com>
2014-11-01 12:28:31 +0000

    On branch master
    This commit is adding GPL headers to each source (*.c and *.h) file.
    
    Changes to be committed:
    
    modified:   raspberry-pi-audio/libilctts/src/audioq.h
    modified:   raspberry-pi-audio/libilctts/src/debug.c
    modified:   raspberry-pi-audio/libilctts/src/debug.h
    modified:   raspberry-pi-audio/libilctts/src/espeak.c
    modified:   raspberry-pi-audio/libilctts/src/espeak.h
    modified:   raspberry-pi-audio/libilctts/src/ilctts_lib.h
    modified:   raspberry-pi-audio/libilctts/src/ilctts_types.h
    modified:   raspberry-pi-audio/libilctts/src/list.c
    modified:   raspberry-pi-audio/libilctts/src/list.h
    modified:   raspberry-pi-audio/libilctts/src/queue.c
    modified:   raspberry-pi-audio/libilctts/src/queue.h
    modified:   raspberry-pi-audio/libilctts/src/ringbuffer.c
    modified:   raspberry-pi-audio/libilctts/src/ringbuffer.h
    modified:   raspberry-pi-audio/libilctts/src/utils.c
    modified:   raspberry-pi-audio/libilctts/src/utils.h

--- raspberry-pi-audio/libilctts/src/queue.h
+++ raspberry-pi-audio/libilctts/src/queue.h
@@ -1,3 +1,27 @@
+/*
+*
+* queue.h - Header file for queue code
+*
+* Header file that contains data type definitions and function prototypes of the queue functionality provided by queue.c.
+*
+* Copyright (C) 2014, Mike Ray, <mike.ray@btinternet.com>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this package; see the file COPYING.  If not, write to the Free
+* Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+* 02110-1301, USA.
+*
+*--code--*/
 #ifndef QUEUE_H
 #define QUEUE_H
 #include <stdlib.h>
--- raspberry-pi-audio/libilctts/src/ringbuffer.c
+++ raspberry-pi-audio/libilctts/src/ringbuffer.c
@@ -1,3 +1,28 @@
+/*
+*
+* ringbuffer.c - Functions to implement a circular buffer of type void
+*
+* Functionality to implement a circular buffer of type void. Differs from
+* that in queue.c in that the FIFO is a pure void data rather than a queue of complex data type pointers.
+*
+* Copyright (C) 2014, Mike Ray, <mike.ray@btinternet.com>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this package; see the file COPYING.  If not, write to the Free
+* Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+* 02110-1301, USA.
+*
+*--code--*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
