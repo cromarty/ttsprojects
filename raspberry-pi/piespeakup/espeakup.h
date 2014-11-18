85a4bf1691b4a77d65afa22966e4c285680c9b55
Mike Ray <mike.ray@btinternet.com>
2014-11-18 14:20:13 +0000

    On branch master Changes to be committed
    new file:   raspberry-pi/piespeakup/.indent.pro
    new file:   raspberry-pi/piespeakup/COPYING
    new file:   raspberry-pi/piespeakup/Makefile
    new file:   raspberry-pi/piespeakup/README
    new file:   raspberry-pi/piespeakup/TODO
    new file:   raspberry-pi/piespeakup/cli.c
    new file:   raspberry-pi/piespeakup/default/piespeakup
    new file:   raspberry-pi/piespeakup/espeak.c
    new file:   raspberry-pi/piespeakup/espeakup.8
    new file:   raspberry-pi/piespeakup/espeakup.h
    new file:   raspberry-pi/piespeakup/piespeakup.8
    new file:   raspberry-pi/piespeakup/piespeakup.c
    new file:   raspberry-pi/piespeakup/queue.c
    new file:   raspberry-pi/piespeakup/queue.h
    new file:   raspberry-pi/piespeakup/signal.c
    new file:   raspberry-pi/piespeakup/softsynth.c
    new file:   raspberry-pi/piespeakup/stringhandling.c
    new file:   raspberry-pi/piespeakup/stringhandling.h

---
+++ raspberry-pi/piespeakup/espeakup.h
@@ -0,0 +1,99 @@
+/*
+ *  espeakup - interface which allows speakup to use espeak
+ *
+ *  Copyright (C) 2008 William Hubbs
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ESPEAKUP_H
+#define __ESPEAKUP_H
+
+/* This was added for gcc 4.3 */
+#include <stddef.h>
+#include <pthread.h>
+
+#include <espeak/speak_lib.h>
+
+#include <ilctts_lib.h>
+
+#include "queue.h"
+
+#define PACKAGE_VERSION "0.80-dev"
+#define PACKAGE_BUGREPORT "http://github.com/williamh/espeakup/issues"
+
+enum espeakup_mode_t {
+	ESPEAKUP_MODE_SPEAKUP,
+	ESPEAKUP_MODE_ACSINT
+};
+
+enum command_t {
+	CMD_SET_FREQUENCY,
+	CMD_SET_PITCH,
+	CMD_SET_PUNCTUATION,
+	CMD_SET_RATE,
+	CMD_SET_VOICE,
+	CMD_SET_VOLUME,
+	CMD_SPEAK_TEXT,
+	CMD_FLUSH,
+	CMD_UNKNOWN,
+};
+
+enum adjust_t {
+	ADJ_DEC,
+	ADJ_SET,
+	ADJ_INC,
+};
+
+struct espeak_entry_t {
+	enum command_t cmd;
+	enum adjust_t adjust;
+	int value;
+	char *buf;
+	int len;
+};
+
+struct synth_t {
+	int frequency;
+	int pitch;
+	int punct;
+	int rate;
+	char voice[10];
+	int volume;
+	char *buf;
+	int len;
+};
+
+extern struct queue_t *synth_queue;
+extern int debug;
+extern enum espeakup_mode_t espeakup_mode;
+
+extern void process_cli(int argc, char **argv);
+extern void *signal_thread(void *arg);
+extern int initialize_espeak(struct synth_t *s);
+extern void *espeak_thread(void *arg);
+extern int open_softsynth(void);
+extern void close_softsynth(void);
+extern void *softsynth_thread(void *arg);
+extern volatile int should_run;
+extern volatile int stop_requested;
+extern int self_pipe_fds[2];
+#define PIPE_READ_FD (self_pipe_fds[0])
+#define PIPE_WRITE_FD (self_pipe_fds[1])
+
+extern pthread_cond_t runner_awake;
+extern pthread_cond_t stop_acknowledged;
+extern pthread_mutex_t queue_guard;
+
+#endif
---
+++ raspberry-pi/piespeakup/signal.c
@@ -0,0 +1,70 @@
+/*
+ *  espeakup - interface which allows speakup to use espeak
+ *
+ *  Copyright (C) 2008 William Hubbs
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#define STOP_MSG "s"
+
+#include "espeakup.h"
+
+/*
+ * We install a dummy signal handler to let the o/s know that we
+ * do not want the default action to be performed since we are
+ * handling the signal.
+ */
+static void dummy_handler(int sig)
+{
+}
+
+void *signal_thread(void *arg)
+{
+	struct sigaction temp;
+	sigset_t sigset;
+	int sig;
+
+	memset(&temp, 0, sizeof (struct sigaction));
+	/* install dummy handlers for the signals we want to process */
+	temp.sa_handler = dummy_handler;
+	sigemptyset(&temp.sa_mask);
+	sigaction(SIGINT, &temp, NULL);
+	sigaction(SIGTERM, &temp, NULL);
+
+	pthread_mutex_lock(&queue_guard);
+	while (should_run) {
+		pthread_mutex_unlock(&queue_guard);
+		sigfillset(&sigset);
+		sigwait(&sigset, &sig);
+		switch (sig) {
+		case SIGINT:
+		case SIGTERM:
+			pthread_mutex_lock(&queue_guard);
+			should_run = 0;
+			pthread_mutex_unlock(&queue_guard);
+			break;
+		default:
+			printf("espeakup caught signal %d\n", sig);
+			break;
+		}
+	}
+	/* Tell the reader to stop, if it is in a select() call. */
+	write(PIPE_WRITE_FD, STOP_MSG, strlen(STOP_MSG));
+	return NULL;
+}
