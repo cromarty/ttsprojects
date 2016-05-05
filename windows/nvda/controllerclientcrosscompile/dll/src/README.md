21f771eb439bdabff279ec898b9569eb8f525111
Mike Ray <mike.ray@btinternet.com>
2016-05-05 07:51:23 +0100

    Changes to nvdaController.h

---
+++ windows/nvda/controllerclientcrosscompile/dll/src/README.md
@@ -0,0 +1,18 @@
+
+# Cross-compilation of NVDA Controller Client
+
+This directory contains files for cross-compiation of the NVDA Controller Client using the mingw 
+compiler for Windows under Linux.
+
+Files:
+
+nvdaController.h
+nvdaController.c
+Makefile.32
+Makefile.64
+
+All self-explanatory.
+
+
+
+
--- windows/nvda/controllerclientcrosscompile/dll/src/nvdaController.h
+++ windows/nvda/controllerclientcrosscompile/dll/src/nvdaController.h
@@ -1,23 +1,32 @@
-
-
-/* this ALWAYS GENERATED file contains the definitions for the interfaces */
-
-
- /* File created by MIDL compiler version 7.00.0555 */
-/* at Fri Feb 19 11:21:40 2010
- */
-/* Compiler settings for interfaces\nvdaController\nvdaController.idl, interfaces\nvdaController\nvdaController.acf:
-    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
-    protocol : dce , ms_ext, c_ext, robust
-    error checks: allocation ref bounds_check enum stub_data 
-    VC __declspec() decoration level: 
-         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
-         DECLSPEC_UUID(), MIDL_INTERFACE()
+/*
+This file is a part of the NVDA project.
+URL: http://www.nvda-project.org/
+Copyright 2006-2010 NVDA contributers.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2.0, as published by
+    the Free Software Foundation.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+This license can be found at:
+http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
-/* @@MIDL_FILE_HEADING(  ) */
 
-#pragma warning( disable: 4049 )  /* more than 64k source lines */
+/*
+A modified version of nvdaController.h.  The original is an automatically generated file.
 
+I have created this version for cross-compilation of the
+NVDA Controller Client with the mingw compiler under Linux.
+
+It removes comments added by the process that generated the original file
+and adds code to either export or import functions depending on build process.
+
+Obviously released under the same version of the GPL as the other components.
+See the licence block above which I culled from the corresponding C file.
+
+Mike Ray, May 2016
+
+*/
 
 /* verify that the <rpcndr.h> version is high enough to compile this file*/
 #ifndef __REQUIRED_RPCNDR_H_VERSION__
@@ -27,10 +36,19 @@
 #include "rpc.h"
 #include "rpcndr.h"
 
+/*
+Add -DCOMPILING_CONTROLLER_CLIENT_DLL to CFLAGS in the Makefile
+used to build the DLL to cause functions to be exported.
+*/
+#ifdef COMPILING_CONTROLLER_CLIENT_DLL
+#define _DECLSPEC_ __declspec(dllexport)
+#else
+#define _DECLSPEC_ __declspec(dllimport)
+#endif
+
 #ifndef __RPCNDR_H_VERSION__
 #error this stub requires an updated version of <rpcndr.h>
 #endif // __RPCNDR_H_VERSION__
-
 
 #ifndef __nvdaController_h__
 #define __nvdaController_h__
@@ -39,41 +57,25 @@
 #pragma once
 #endif
 
-/* Forward Declarations */ 
-
 #ifdef __cplusplus
 extern "C"{
 #endif 
 
-
 #ifndef __NvdaController_INTERFACE_DEFINED__
 #define __NvdaController_INTERFACE_DEFINED__
 
-/* interface NvdaController */
-/* [implicit_handle][version][uuid] */ 
-
-/* [comm_status][fault_status] */ error_status_t __stdcall nvdaController_testIfRunning( void);
-
-/* [comm_status][fault_status] */ error_status_t __stdcall nvdaController_speakText( 
-    /* [string][in] */ const wchar_t *text);
-
-/* [comm_status][fault_status] */ error_status_t __stdcall nvdaController_cancelSpeech( void);
-
-/* [comm_status][fault_status] */ error_status_t __stdcall nvdaController_brailleMessage( 
-    /* [string][in] */ const wchar_t *message);
-
+error_status_t __stdcall _DECLSPEC_ nvdaController_testIfRunning( void);
+error_status_t __stdcall _DECLSPEC_ nvdaController_speakText(const wchar_t *text);
+error_status_t __stdcall _DECLSPEC_ nvdaController_cancelSpeech( void);
+error_status_t __stdcall _DECLSPEC_ nvdaController_brailleMessage(const wchar_t *message);
 
 extern handle_t nvdaControllerBindingHandle;
-
-
 extern RPC_IF_HANDLE nvdaController_NvdaController_v1_0_c_ifspec;
 extern RPC_IF_HANDLE NvdaController_v1_0_c_ifspec;
 extern RPC_IF_HANDLE nvdaController_NvdaController_v1_0_s_ifspec;
 #endif /* __NvdaController_INTERFACE_DEFINED__ */
 
-/* Additional Prototypes for ALL interfaces */
 
-/* end of Additional Prototypes */
 
 #ifdef __cplusplus
 }
