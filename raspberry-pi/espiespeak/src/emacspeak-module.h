613d2571123edbdc805a3563b75bc5fef44eb0a3
Mike Ray <mike.ray@btinternet.com>
2017-01-01 20:47:58 +0000

    deleted:    generic/emacspeak/README 	deleted:    generic/emacspeak/bison-parser/emacspeak.y 	renamed:    generic/emacspeak/esproxy/README.md -> generic/emacspeak/esproxy/README 	deleted:    generic/emacspeak/flex-lexer/emacspeak.l 	modified:   generic/emacspeak/include/emacspeakproto.h 	modified:   generic/emacspeak/parser/README 	modified:   raspberry-pi/espiespeak/src/emacspeak-module.h 	new file:   raspberry-pi/espiespeak/src/emacspeakproto.h

--- raspberry-pi/espiespeak/src/emacspeak-module.h
+++ raspberry-pi/espiespeak/src/emacspeak-module.h
@@ -1,27 +1,26 @@
 /*
- *
- * Copyright (C) 2014 Mike Ray <mike.ray.1964@gmail.com>
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this package; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
- *
- * $Id: emspk-sd.h
- */
-
-#ifndef EMSPK_SD_H
-#define EMSPK_SD_H
+*
+* Copyright (C) 2014 Mike Ray <mike.ray@btinternet.com>
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this package; see the file COPYING.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301, USA.
+*
+* $Id: emspk-sd.h
+*/
+#ifndef EMACSPEAK_PROTO_H
+#define EMACSPEAK_PROTO_H
 
 #define NO_SYNC -1
 
