3d6de995763207324eb91815e45e2fbc5e76bb16
Mike Ray <mike.ray@btinternet.com>
2017-01-26 19:27:11 +0000

    On branch master Changes to be committed:
    Added list and queue to x86test
    
    	modified:   generic/emacspeak/x86test/src/list.c
    	modified:   generic/emacspeak/x86test/src/list.h
    	modified:   generic/emacspeak/x86test/src/queue.c
    	modified:   generic/emacspeak/x86test/src/queue.h

--- generic/emacspeak/x86test/src/queue.h
+++ generic/emacspeak/x86test/src/queue.h
@@ -1,3 +1,25 @@
+/*
+*
+* Copyright (C) 2017 Mike Ray <mike.ray@btinternet.com>
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this package; see the file COPYING.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301, USA.
+*
+* $Id: emspk-sd.h
+*--code--*/
+
 #ifndef QUEUE_H
 #define QUEUE_H
 
@@ -10,10 +32,11 @@ typedef List Queue;
 #define queue_init list_init
 #define queue_destroy list_destroy
 
-int queue_enqueue(Queue *queue, const void *data);
-int queue_dequeue(Queue *queue, void **data);
+int queue_push(Queue *queue, const void *data);
+int queue_pop(Queue *queue, void **data);
 
 #define queue_peek(queue) ((queue)->head == NULL ? NULL : (queue)->head->data)
 #define queue_size list_size
 
 #endif
+
